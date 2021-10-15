dc0697df18291fa65460b9c33466705f9f6cf24e
pcsmomo <dongwoo_333@hanmail.net>
2021-10-15 14:34:51 +1100

    cleanup for the next lecture

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-// import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Container from '@material-ui/core/Container';
-// import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: 'violet',
-    '&:hover': {
-      background: 'blue',
-    },
-  },
-  title: {
-    textDecoration: 'underline',
-    marginBottom: 20,
-  },
-});
+const useStyles = makeStyles({});
 
 export default function Create() {
   const classes = useStyles();
@@ -27,7 +13,6 @@ export default function Create() {
   return (
     <Container>
       <Typography
-        className={classes.title}
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -35,47 +20,16 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
-      {/* Create page
-      <Typography>Create a New Note</Typography>
-      <Typography variant="h1" color="primary" align="center">
-        Create a New Note
-      </Typography>
-      <Typography noWrap color="secondary">
-        Ex id cupidatat nostrud proident cupidatat est Lorem tempor qui. Ut
-        culpa incididunt reprehenderit deserunt enim labore commodo do. Nisi
-        ullamco do laboris et. Reprehenderit magna do esse ex non. Reprehenderit
-        nostrud adipisicing et elit proident sint.
-      </Typography> */}
 
       <Button
-        className={classes.btn}
         onClick={() => console.log('you clicked me')}
         type="submit"
-        color="secondary"
+        color="primary"
         variant="contained"
-        // startIcon={<SendIcon />}
         endIcon={<KeyboardArrowRight />}
       >
         Submit
       </Button>
-      {/* <Button type="submit">Submit</Button>
-      <Button type="submit" color="secondary" variant="outlined">
-        Submit
-      </Button>
-      <ButtonGroup color="secondary" variant="contained">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup> */}
-
-      {/* icons */}
-      {/* <br />
-      <AcUnitOutlinedIcon />
-      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
-      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
-      <AcUnitOutlinedIcon color="action" fontSize="small" />
-      <AcUnitOutlinedIcon color="error" fontSize="small" />
-      <AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
     </Container>
   );
 }
