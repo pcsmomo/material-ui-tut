e7785f1a6c634ef1dd160aba740662f2a759ade0
pcsmomo <dongwoo_333@hanmail.net>
2021-10-15 13:12:06 +1100

    2. Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 export default function Create() {
-  return <div>Create page</div>;
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+      {/* Create page
+      <Typography>Create a New Note</Typography>
+      <Typography variant="h1" color="primary" align="center">
+        Create a New Note
+      </Typography>
+      <Typography noWrap color="secondary">
+        Ex id cupidatat nostrud proident cupidatat est Lorem tempor qui. Ut
+        culpa incididunt reprehenderit deserunt enim labore commodo do. Nisi
+        ullamco do laboris et. Reprehenderit magna do esse ex non. Reprehenderit
+        nostrud adipisicing et elit proident sint.
+      </Typography> */}
+    </div>
+  );
 }
